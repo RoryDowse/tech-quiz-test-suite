9b542d8af138a4f94cfa2ef3ebd75da966a50f74
Rory Dowse <rorydowse@hotmail.com>
2024-10-21 14:38:02 -0700

    add comments to cypress.config.ts

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress';
+// Import custom Vite config for component testing
+import customViteConfig from './vite.config'; 
+
+export default defineConfig({
+    // Configuration for component testing
+    component: {
+        devServer: {
+            framework: 'react',
+            bundler: 'vite',
+            viteConfig: customViteConfig, // Pass the custom Vite configuration (from vite.config.ts)
+        },
+        // Specify the pattern to locate component test files
+        specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
+    },
+
+    // Configuration for end-to-end testing
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
+});
